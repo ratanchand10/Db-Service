78f5fc620ac6d683ac481ddab5ed3b674513af52
ratanchand10 <ratanchandya10@gmail.com>
2019-05-22 19:11:55 +0530

    db-service Commit

--- Db-Service/src/main/java/com/techprimers/stock/dbservice/DbServiceApplication.java
+++ Db-Service/src/main/java/com/techprimers/stock/dbservice/DbServiceApplication.java
@@ -2,10 +2,10 @@ package com.techprimers.stock.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.techprimers.stock.dbservice.repository")
 @SpringBootApplication
 public class DbServiceApplication {
